c182a1d51727d814ec50b1b5d6942945d14bf742
StoopDJ <amirakak@gmail.com>
2020-04-22 10:23:29 +0100

    Task

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -1,5 +1,7 @@
 package ie.tudublin;
 
+import processing.data.TableRow;
+
 public class Task {
     private String Tasks;
     private int StartDate;
@@ -17,10 +19,26 @@ public class Task {
     public int getStartDate(){
         return StartDate;
     }
+
     public void setStartDate(){
         this.StartDate = StartDate;
     }
+
     public int getEndDate(){
         return EndDate;
+    }
+
+    public void setEndDate(){
+        this.EndDate = EndDate;
+    }
+
+    public Task (String Tasks, int StartDate, int EndDate){
+        this.Tasks = Tasks;
+        this.StartDate = StartDate;
+        this.EndDate = EndDate;
+    }
+
+    public Task(TableRow tr){
+        this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
     }
 }
