b50dfa153ed6227fc2e71e8aaa0394317cac748c
StoopDJ <amirakak@gmail.com>
2020-04-22 12:25:59 +0100

    Gantt display() HSB rects is added

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -40,8 +40,14 @@ public class Gantt extends PApplet
 	public void displayTasks(){
 		float X = width * 0.05f;
 		float margin = width / 20;
-	
 		float y;
+		float rectH = 40;
+		float rectS, rectE, rectW;
+		float color;
+		float leftBorder = width / 6;
+		float textX = X * 3.5f;
+		float textY = height * 0.08f;
+		int textSpace = 21;
 	
 		//Tasks
 		for(int i = 0; i < tasks.size(); i++)
@@ -49,17 +55,25 @@ public class Gantt extends PApplet
 			fill(255);
 			y = map(i, 0, tasks.size(), 2 * margin, height - margin);
 			text(tasks.get(i).getTasks(), margin, y);
+
+			// drawing rects for each tasks
+
+				//HSB color
+				smooth();
+				noStroke();
+				color = map(i, 0, tasks.size(), 0, 255);
+				fill(color, 255, 255);
+				
+				rectS = map(tasks.get(i).getStartDate(), 1, 30, leftBorder, width - margin);
+				rectE = map(tasks.get(i).getEndDate(), 1, 30, leftBorder, width - margin);
+				rectW = rectE - rectS;
+				rect(rectS, y - rectH / 2, rectW, rectH);
 		}
 		
 
 		// lines and numbers
-		float textX = X * 3.5f;
-		float textY = height * 0.08f;
-		int textSpace = 21;
-
 		fill(255);
 
-		
 		for(int count = 1; count < 31; count++)
 		{
 			if(count % 2 == 0)
@@ -75,10 +89,8 @@ public class Gantt extends PApplet
 			text(count, textX, textY - textSpace);
 			textX += textSpace;
 		}
-	
-	}
-	
-
+		
+	}		
 	
 	public void mousePressed()
 	{
